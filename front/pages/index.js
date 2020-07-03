60f4c55ec9f6d3b565784d281c4b165f7644b9a1
KamehaMehdi <dahmammehdi@gmail.com>
2020-07-03 05:04:52 +0200

    Adding graphql and query React method

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
 
 import { Dashboard } from '../src/dashboard';
 
 
-
 export default () => {
 
+  const client = new ApolloClient({
+    uri: 'http://127.0.0.1:8765/ql'
+  })
+
   return (
-    <div style={{display:'flex', justifyContent:'center'}}>
-      <Dashboard/>
-    </div>
+    <ApolloProvider client={client}>
+      <div style={{display:'flex', justifyContent:'center', marginTop: 200}}>
+        <Dashboard/>
+      </div>
+    </ApolloProvider>
   );
 }
